33aa5fc602d6af625da768b4286116efae813647
Athul2530 <athul8741@gmail.com>
2025-05-28 23:07:07 +0530

    Add project files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Portfolio/', // 👈 Add this line
+  base: '/portfolio_Athul/', // 👈 IMPORTANT
   plugins: [react()],
 })
